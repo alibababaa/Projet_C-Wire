718c4efaa92f8ab80a7298077506c6f8b466d65d
alibababaa <yildizaliulas@gmail.com>
2024-12-22 20:31:27 +0100

    Update avl_tree.h

--- codeC/avl_tree.h
+++ codeC/avl_tree.h
@@ -2,7 +2,10 @@
 #define AVL_TREE_H
 
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
+#include <errno.h>
+
 
 typedef struct NoeudAVL {
     int identifiant;
@@ -20,12 +23,25 @@ typedef struct {
 } StationData;
 
 // Fonctions principales de l'AVL
+static int max(int a, int b);
+static int hauteur(NoeudAVL* n);
+static NoeudAVL* creerNoeud(int identifiant, long capacite, long consommation);
+static int facteurEquilibre(NoeudAVL* n);
+static NoeudAVL* rotationDroite(NoeudAVL* y);
+static NoeudAVL* rotationGauche(NoeudAVL* x);
+static NoeudAVL* equilibrer(NoeudAVL* noeud);
 NoeudAVL* inserer(NoeudAVL* racine, int identifiant, long capacite, long consommation);
 NoeudAVL* rechercher(NoeudAVL* racine, int identifiant);
 void libererAVL(NoeudAVL* noeud);
-
-// Fonctions utilitaires pour les résultats
+static void count_nodes(NoeudAVL* n, int* compteur);
+static void avl_to_array_infixe(NoeudAVL* racine, StationData* tab, int* index);
 StationData* avl_to_array(NoeudAVL* racine, int* count);
 int comparer_par_capacite(const void* a, const void* b);
+
+
+// Fonctions utilitaires pour les graphiques
+void erreurCritique(const char* message);
+void process_csv(const char* filename, const char* output_filename);
+
 
 #endif
